e8bda30fd988d6f83c46580536a6a66d506bde19
Ruthrakumar <Ruthrakumar@users.noreply.github.com>
2018-09-14 10:48:04 +0530

    just next

--- init/main.go
+++ init/main.go
@@ -1,5 +1,18 @@
 package main
-import "fmt"
-func main(){
-	fmt.Println("hello");
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func indexhandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello web billionere is back")
+}
+func move(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "millionere's  billionere")
+}
+func main() {
+	http.HandleFunc("/", indexhandler)
+	http.HandleFunc("/keepMoving", move)
+	http.ListenAndServe(":8000", nil)
 }
